985d3fb067adb09bd68cc0e622099f6fc6fec3ce
Muskan Batra <68785899+muskan-batra@users.noreply.github.com>
2023-02-27 18:47:17 +0500

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -7,9 +7,10 @@ import React,{useState} from 'react';
 import { Row,Col } from 'react-bootstrap';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+
 const people = [
   {
-    name: "Gramatical Standard English",
+    name: "GRAMATICAL STANDARD ENGLISH",
     Summary: "Correct this to standard English: She no went to the market.",
   },
   {
@@ -21,22 +22,44 @@ const people = [
   Summary: "Convert this text to a programmatic command: Example: Ask Constance if we need some bread  Output: send-msg `find constance` Do we need some bread? Reach out to the ski store and figure out if I can get my skis fixed before I leave on Thursday",
   },
   {
-    name: "Gramatical Standard English",
-    Summary: "Correct this to standard English: She no went to the market.",
+    name: "Q&A",
+    Summary: "I am a highly intelligent question answering bot. If you ask me a question that is rooted in truth, I will give you the answer. If you ask me \n a question that is nonsense, trickery, or has no clear answer, I will respond with  \n  Q: What is human life expectancy in the United States? \n A: Human life expectancy in the United States is 78 years.  \n Q: Who was president of the United States in 1955? \n  A: Dwight D. Eisenhower was president of the United States in 1955. \n   Q: Which party did he belong to? \n  A: He belonged to the Republican Party."
+},
+  {
+    name: "ENGLISH TO OTHER LANGUAGES",
+    Summary : "Translate this into \n 1. French, \n 2. Spanish \n  3. Japanese:"
   },
   {
-    name: "Summarize for second Grader",
-    Summary : "Summarize this for a second-grade student: Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus."
+    name: "PARSED UNSTRUCTURED DATA",
+  Summary: "A table summarizing the fruits from Goocrux: \n\n\n There are many fruits that were found on the recently discovered planet Goocrux. There are neoskizzles that grow there, which are purple and taste like candy. There are also loheckles, which are a grayish blue fruit and are very tart, a little bit like a lemon. Pounits are a bright green color and are more savory than sweet. There are also plenty of loopnovas which are a neon pink flavor and taste like cotton candy. Finally, there are fruits called glowls, which have a very sour and bitter taste which is acidic and caustic, and a pale orange tinge to them.",
   },
   {
-    name: "Text To Command",
-  Summary: "Convert this text to a programmatic command: Example: Ask Constance if we need some bread  Output: send-msg `find constance` Do we need some bread? Reach out to the ski store and figure out if I can get my skis fixed before I leave on Thursday",
-  },
-];
+  name: "Classification",
+  Summary: "The following is a list of companies and the categories they fall into: \n Apple, Facebook, Fedex \n Apple \n  Category",
+},
 
+{
+    name: "Natural languages to python",
+    Summary: " \n''' \n  1. Create a list of first names \n  2. Create a list of last names \n 3. Combine them randomly into a list of 100 full names \n''' \n",
+},
+
+{
+    name: "Chat",
+    Summary: "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n Human: Hello, who are you?\n AI: I am an AI created by OpenAI. How can I help you today?\n Human:.... ",
+},
+{
+    name: "More Examples",
+   
+},
+];
+const styleObj = {
+    fontSize: 15
+    
+}
  
   
 function Page() {
+   
  
   const [option, setOption] = React.useState(null);
   const [text, setText] = React.useState("");
@@ -58,15 +81,18 @@ function Page() {
      <Container fluid style={{width : "70%"}}>
       
      <Navbar bg="light" expand="lg" style={{height: "70px" , borderColor: "black" ,border : "bottom"}}>
+     <img class="img-navbar" src="https://backend.dsl.sg/public/1674984782773.png" alt="logo" width={60} style={{marginleft:"1000px"}}></img>
       <Container fluid>
+
         <Navbar.Brand href="#"></Navbar.Brand>
+       
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0"
             style={{ maxHeight: '100px' }}
             navbarScroll
-          ><img class="img-navbar" src="https://backend.dsl.sg/public/1674984782773.png" alt="logo" width={70 } style={{marginRight:"30px"}}></img>
+          >
            <NavDropdown.Divider />
                </Nav>
           <Form className="d-flex">
@@ -80,22 +106,19 @@ function Page() {
     <hr/>
    <div className='page-header'>
     <h4 class="pg-page-title" style={{marginTop: "40px" }}>
-      AI Tester
+      AI TESTER
 
-<select value={option} onChange={onChange} style= {{marginLeft :'450px' , height :'35px',marginBottom:  '10px' ,  size:"1rem"}}  >
+<select value={option} onChange={onChange} style= {{marginLeft :'435px' ,width :" 41%" , height :'35px',marginBottom:  '10px' , fontSize: '20px' }} defaultValue={people[1]}  >
 
-
-
- <option selected disabled hidden>
- 
- Load a Preset
- </option>
  
    {people.map((person, index) => (
     
-     <option value={index} key={index}>
-       {person.name}
-     </option>
+     <><option value={index} key={index}>
+           {person.name}
+
+       </option>
+      
+       </>
      
    ))}
  </select>
@@ -104,11 +127,13 @@ function Page() {
       <div class="ml-7">
         <textarea class="form-control" 
   id="exampleFormControlTextarea1" 
-  rows="15"
+  rows="13"
   cols= "" 
   bor
-  placeholder= ' Write a tagline for icecream shop'
+  fontSize= '35px'
+  placeholder= ' WRITE A TAGLINE FOR ICE-CREAM SHOP'
   value={text}
+  style={styleObj}
   onChange = {(ev) => setText(ev.target.value)}
   > </textarea>
 
@@ -116,7 +141,9 @@ function Page() {
 <br/>
 
             <div class="position-absolute top-left">
-                <button type="button" class="btn btn-primary">Submit</button>
+            <Button variant="primary" size="lg" active>
+          SUBMIT
+        </Button>
             </div>
 
     </Container>
